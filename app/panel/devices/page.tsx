6333315c44aed304f477e8891260752d8f9cff44
Ibrahim Okić <70588174+kaizerpwn@users.noreply.github.com>
2023-09-15 19:57:30 +0200

    Server side rendering removed
    - Removed old code for server-side rendering, need to implement new SSR with app router

--- app/panel/devices/page.tsx
+++ app/panel/devices/page.tsx
@@ -1,26 +1,8 @@
-'use client'
+"use client";
 
-import Breadcrumb from '@/components/Panel/Breadcrumb/Breadcrumb'
-import Devices from '@/components/Panel/Devices/Devices'
-import PanelLayout from '@/components/Panel/PanelLayout'
-import Axios from '@/utils/Axios'
-import { QueryClient, QueryKey } from '@tanstack/react-query'
-import { GetServerSideProps } from 'next'
-import DevicesInterface from '@/utils/Interfaces/Devices'
-
-const DEVICES_QUERY_KEY: QueryKey = ['devices'];
-const AxiosInstance = new Axios();
-export const queryClient = new QueryClient();
-
-export const getServerSideProps: GetServerSideProps<{ devices: DevicesInterface[] }> = async (context) => {
-  await queryClient.prefetchQuery(DEVICES_QUERY_KEY, AxiosInstance.getAllDevices);
-
-  return {
-    props: {
-      devices: queryClient.getQueryData(DEVICES_QUERY_KEY) as DevicesInterface[],
-    },
-  };
-}
+import Breadcrumb from "@/components/Panel/Breadcrumb/Breadcrumb";
+import Devices from "@/components/Panel/Devices/Devices";
+import PanelLayout from "@/components/Panel/PanelLayout";
 
 interface Device {
   id: number;
@@ -39,7 +21,7 @@ const DevicesPage = ({ devices }: DeviceProps) => {
         <Devices devices={devices} />
       </PanelLayout>
     </>
-  )
-}
+  );
+};
 
-export default DevicesPage
+export default DevicesPage;
